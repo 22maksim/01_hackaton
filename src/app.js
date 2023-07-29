aef29f0c3e4951d64c75c654a498386273c1b24b
Alexander <3724198@mail.ru>
2023-07-29 15:51:26 +0300

    init context menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,32 @@
-import './styles.css'
+import './styles.css';
+import { ContextMenu } from './menu';
 
+const contextMenu = new ContextMenu();
+
+function handleContextMenu(event) {
+  event.preventDefault();
+  contextMenu.open(event.clientX, event.clientY);
+  console.log(contextMenu);
+  console.log(event);
+}
+
+document.addEventListener('contextmenu', handleContextMenu);
+
+class Figure {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const figure1 = new Figure('Создать Фигуру');
+//const figure2 = new Figure('Фигура 2');
+
+contextMenu.add(figure1);
+//contextMenu.add(figure2);
+
+function handleClick() {
+  contextMenu.close();
+  console.log(contextMenu);
+}
+
+document.addEventListener('click', handleClick);
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,51 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
+  constructor() {
+    super();
+    this.items = [];
+    this.menuElement = null;
+  }
 
+  open(x, y) {
+    if (this.items.length === 0) {
+      return;
+    }
+    if (!this.menuElement) {
+      this.createMenu();
+    }
+
+    this.menuElement.style.left = x + 'px';
+    this.menuElement.style.top = y + 'px';
+    this.menuElement.style.display = 'block';
+  }
+
+  close() {
+    if (this.menuElement) {
+      this.menuElement.style.display = 'none';
+    }
+  }
+
+  add(item) {
+    this.items.push(item);
+  }
+
+  createMenu() {
+    // Создаем DOM элемент для меню
+    this.menuElement = document.createElement('ul');
+    this.menuElement.className = 'context-menu';
+    // Добавляем элементы меню в DOM
+    for (let item of this.items) {
+      const menuItem = document.createElement('li');
+      menuItem.textContent = item.name;
+      menuItem.addEventListener('click', () => {
+        // Вызываем метод trigger() соответствующего модуля при клике на пункт меню
+        item.trigger();
+        this.close();
+      });
+      this.menuElement.appendChild(menuItem);
+    }
+    // Добавляем меню в DOM дерево
+    document.body.appendChild(this.menuElement);
+  }
 }
